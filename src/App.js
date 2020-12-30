352c9416b16107894edc3c492546b888dddd6987
trung <trungxp32290@gmail.com>
2020-12-30 21:27:18 +1100

    List / useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { useState }  from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
-function App() {
+function App() { 
+
+  const [todoItems, setTodoItems] = useState ([
+    {id:1, title: 'Action 1'},
+    {id:2, title: 'Action 2'},
+    {id:3, title: 'Action 3'}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <h1> To do List</h1>
+      <TodoList list={todoItems} />
     </div>
+
   );
-}
+ }
 
 export default App;
---
+++ src/components/TodoList/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoList.propTypes = {
+    list : PropTypes.array,
+    onClick : PropTypes.func
+    
+};
+
+TodoList.defaultProps = {
+    list : [],
+    onClick: null
+}
+
+function TodoList(props) {
+    
+    const { list, onClick } = props;
+    
+    return (
+        <ul>
+        {list.map( (item) => 
+            <li key={item.id}> 
+                {item.title}
+            </li>
+        )}
+        </ul>
+    );
+}
+
+export default TodoList;
